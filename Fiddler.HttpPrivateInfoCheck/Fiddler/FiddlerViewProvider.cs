b7fe156787af682b4c3cb2d0f32bba9d470896b7
Jasongrass <junjiegrass@qq.com>
2021-07-12 17:02:36 +0800

    初始化项目

---
+++ Fiddler.HttpPrivateInfoCheck/Fiddler/FiddlerViewProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fiddler.HttpPrivateInfoCheck.View;
+using Jgrass.FiddlerPlugin;
+
+namespace Fiddler.HttpPrivateInfoCheck.Fiddler
+{
+    class FiddlerViewProvider: IFiddlerViewProvider
+    {
+        public IList<FiddlerTabPage> BuildFiddlerTabPages()
+        {
+            return BuildFiddlerViews();
+        }
+
+        public IList<FiddlerTabPage> BuildFiddlerViews()
+        {
+            var page = new FiddlerTabPage("HTTP 敏感信息监测", new MainView())
+            {
+                TabIcon = SessionIcons.Information
+            };
+
+            return new List<FiddlerTabPage>() {page};
+        }
+    }
+}
---
+++ Fiddler.HttpPrivateInfoCheck/Fiddler/HttpContentCheckFiddlerPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fiddler;
+using Jgrass.FiddlerPlugin;
+
+[assembly: RequiredVersion("2.1.8.1")]
+
+namespace Fiddler.HttpPrivateInfoCheck.Fiddler
+{
+    public class HttpContentCheckFiddlerPlugin: FiddlerPluginApplication
+    {
+        public override IFiddlerViewProvider GetFiddlerViewProvider()
+        {
+            return new FiddlerViewProvider();
+        }
+    }
+}
