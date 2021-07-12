e50cb09a7ca82a07ebbc63364db7d1f67db402f2
Jasongrass <junjiegrass@qq.com>
2021-07-12 19:07:27 +0800

    添加基本的数据结构

---
+++ Fiddler.HttpPrivateInfoCheck/Configurations/CheckRule.cs
@@ -0,0 +1,52 @@
+﻿#pragma warning disable MA0048 // File name must match type name
+
+
+namespace Fiddler.HttpPrivateInfoCheck.Configurations
+{
+    /// <summary>
+    /// 匹配的规则
+    /// </summary>
+    class CheckRule
+    {
+        /// <summary>
+        /// 用于配置的值
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// 用于匹配的模式
+        /// </summary>
+        public string MatchType { get; set; }
+
+        /// <summary>
+        /// 是否匹配请求
+        /// </summary>
+        public bool IsMatchRequest { get; set; } = true;
+
+        /// <summary>
+        /// 是否匹配响应
+        /// </summary>
+        public bool IsMatchResponse { get; set; } = true;
+
+        /// <summary>
+        /// 出现匹配时给用户的提示
+        /// </summary>
+        public string Message { get; set; }
+    }
+
+    /// <summary>
+    /// 匹配计算的模式
+    /// </summary>
+    enum RuleMatchType
+    {
+        /// <summary>
+        /// 字符串比较
+        /// </summary>
+        String,
+
+        /// <summary>
+        /// 正则匹配
+        /// </summary>
+        Regex,
+    }
+}
---
+++ Fiddler.HttpPrivateInfoCheck/Configurations/GlobalConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiddler.HttpPrivateInfoCheck.Configurations
+{
+    /// <summary>
+    /// 全局配置
+    /// </summary>
+    class GlobalConfiguration
+    {
+        /// <summary>
+        /// 需要进行检查的匹配域名
+        /// </summary>
+        public IList<string> MatchHosts { get; set; }
+
+        /// <summary>
+        /// 匹配检查的规则
+        /// </summary>
+        public IList<CheckRule> CheckRules { get; set; }
+
+    }
+}
