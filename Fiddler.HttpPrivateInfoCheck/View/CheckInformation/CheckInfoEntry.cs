4d56d6aa8b5a0fac9257b77d62a06557b4d1b86e
Jasongrass <junjiegrass@qq.com>
2021-07-30 16:46:21 +0800

    添加结果页去重的功能

---
+++ Fiddler.HttpPrivateInfoCheck/View/CheckInformation/CheckInfoEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiddler.HttpPrivateInfoCheck.View.CheckInformation
+{
+    /// <summary>
+    /// 检查结果信息
+    /// </summary>
+    class CheckInfoEntry
+    {
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public string DetailTitle { get; set; }
+
+        public string DetailContent { get; set; }
+    }
+}
--- Fiddler.HttpPrivateInfoCheck/View/CheckInformation/CheckInformationView.xaml.cs
+++ Fiddler.HttpPrivateInfoCheck/View/CheckInformation/CheckInformationView.xaml.cs
@@ -22,8 +22,11 @@ namespace Fiddler.HttpPrivateInfoCheck.View.CheckInformation
     /// </summary>
     public partial class CheckInformationView : UserControl, ICheckInformationView
     {
+        private IList<HttpCheckInfo> CheckInfos { get; }
+
         public CheckInformationView()
         {
+            CheckInfos = new List<HttpCheckInfo>();
             InitializeComponent();
             Container.View = this;
         }
@@ -37,12 +40,55 @@ namespace Fiddler.HttpPrivateInfoCheck.View.CheckInformation
                 DetailTitle = info.RequestUrl,
                 DetailContent = info.Detail
             };
+            view.Tag = info;
+
+            CheckInfos.Add(info);
             HttpCheckInfosWaterfallPanel.Children.Add(view);
         }
 
+        //清空全部
         private void ClearButton_OnClick(object sender, RoutedEventArgs e)
         {
             HttpCheckInfosWaterfallPanel.Children.Clear();
+            CheckInfos.Clear();
+        }
+
+        /// <summary>
+        /// 去除重复
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DistinctButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            IList<HttpCheckInfo> remainInfos = new List<HttpCheckInfo>();
+
+            IList<UIElement> removeViews = new List<UIElement>();
+
+            foreach (UIElement child in HttpCheckInfosWaterfallPanel.Children)
+            {
+                if (child is CheckInfoCardView { Tag: HttpCheckInfo info })
+                {
+                    if (remainInfos.Any(r => IsSame(r, info)))
+                    {
+                        removeViews.Add(child);
+                        CheckInfos.Remove(info);
+                        continue;
+                    }
+                    remainInfos.Add(info);
+                }
+            }
+
+            foreach (var view in removeViews)
+            {
+                HttpCheckInfosWaterfallPanel.Children.Remove(view);
+            }
+
+        }
+
+        private bool IsSame(HttpCheckInfo info1, HttpCheckInfo info2)
+        {
+            return string.Equals(info1.RequestUrl, info2.RequestUrl, StringComparison.Ordinal) &&
+                   string.Equals(info1.Message, info2.Message, StringComparison.Ordinal);
         }
 
     }
